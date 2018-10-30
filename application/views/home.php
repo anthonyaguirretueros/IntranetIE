a2c5e9d8afd4874621e8325492931d2226389b50
anthonyaguirretueros <38079518+anthonyaguirretueros@users.noreply.github.com>
2018-10-30 10:46:08 -0500

    intranet

---
+++ application/views/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>felicidades usted se ha logueado</h1>
+	<table>
+		<tr>
+			<td>Usuario : </td>
+			<td><input class="user"></input></td>
+
+		</tr>
+		<tr>
+			<td>Contraseña :</td>
+			<td><input class="pass"></input></td>
+
+		</tr>
+
+	</table>
+</body>
+</html>
