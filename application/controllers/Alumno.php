75554ff238395bbf88228ed1859f46191ca2b7c2
anthonyaguirretueros <38079518+anthonyaguirretueros@users.noreply.github.com>
2018-11-06 16:27:41 -0500

    GuardarAlumno

---
+++ application/controllers/Alumno.php
@@ -0,0 +1,44 @@
+<?php 
+
+	defined('BASEPATH') OR exit('No direct  script access allowed');
+
+class alumno extends CI_Controller
+{
+
+	function __construct()
+	{
+		parent :: __construct();	
+		$this->load->model("m_alumno");
+	}
+	public function index()
+	{
+		$this->load->model("m_alumno");
+		$alumnos =$this->m_alumno->getalumno();
+		//print_r($alumnos);
+		//echo json_encode($alumnos);
+		
+		$this->load->view("alumnos",compact("alumnos"));
+	}
+
+	public function GuardarAlumno()
+	{
+		if ($this->input->post()) 
+		{
+		
+		// echo "<pre>";	print_r($_POST);die();echo "</pre>";
+		
+		$nombre =$this->db->escape($_POST["nombre"]);
+	
+		$apellido =$this->db->escape($_POST["apellido"]);
+			//print_r($apellido);die();
+		$cumpleanos =$this->db->escape($_POST["cumpleanos"]);
+		$grado =$this->db->escape($_POST["grado"]);
+		$nivel =$this->db->escape($_POST["nivel"]);
+		$this->m_alumno->setalumno($nombre,$apellido,$cumpleanos,$grado,$nivel);	
+
+			// $alumno = $this->input->post();
+			// echo json_encode($alumno);
+		}
+	}
+}
+ ?>
