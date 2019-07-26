7a16c00396780b49cadcc0fd917c6d62fdf2e9b1
Luis David <luisdavidgd@gmail.com>
2019-07-26 11:56:42 -0500

    change db config

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'intranet-mysql',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'intranet',
