d18f8d46bce1c2f9c281839bc7babb8c310c0cfe
Luis David <luisdavidgd@gmail.com>
2019-07-26 17:36:32 -0500

    Update home controller and some views

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,6 @@ class Home extends MY_Controller
     public function index()
     {
         $data = $this->ion_auth_data;
-        $this->load->twig('templates/adminlte', $data);
+        $this->load->twig('home', $data);
     }
 }
