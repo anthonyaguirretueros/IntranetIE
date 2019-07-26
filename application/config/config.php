10ccf44b8f065d1bad9bd14aa63e9d88e29769f6
Luis David <luisdavidgd@gmail.com>
2019-07-26 13:57:16 -0500

    Update config file

--- application/config/config.php
+++ application/config/config.php
@@ -27,7 +27,7 @@ $config['base_url'] = getenv('BASE_URL');
 
 
 
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
